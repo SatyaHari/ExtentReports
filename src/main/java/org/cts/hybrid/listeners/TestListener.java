6e4ee9e6ec34be885784b8321bb8fa49a69743c1
hp <dipangarg92@gmail.com>
2018-08-15 16:58:58 +0530

    logger added. AssertionLibrary moved out to Utilities project.

--- src/main/java/org/cts/hybrid/listeners/TestListener.java
+++ src/main/java/org/cts/hybrid/listeners/TestListener.java
@@ -5,11 +5,14 @@ import org.cts.hybrid.ExtentReports.ExtentTestManager;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.log4testng.Logger;
 
 import com.aventstack.extentreports.Status;
 
 public class TestListener implements ITestListener {
 
+	private static final  Logger logger=Logger.getLogger(TestListener.class);
+	
 	private static String getTestMethodName(ITestResult iTestResult) {
 		return iTestResult.getMethod().getConstructorOrMethod().getName();
 	}
@@ -19,43 +22,38 @@ public class TestListener implements ITestListener {
 	}
 
 	public void onStart(ITestContext iTestContext) {
-		System.out.println("I am in onStart method " + iTestContext.getName());
-		// iTestContext.setAttribute("WebDriver", this.driver);
+		logger.info("I am in onStart method " + iTestContext.getName());
 	}
 
 	public void onFinish(ITestContext iTestContext) {
-		System.out.println("I am in onFinish method " + iTestContext.getName());
-		// Do tier down operations for extentreports reporting!
-		// ExtentTestManager.endTest();
+		logger.info("I am in onFinish method " + iTestContext.getName());
 		ExtentManager.getInstance().flush();
 		ExtentTestManager.endTest();
 	}
 
 	public void onTestStart(ITestResult iTestResult) {
-		System.out.println("I am in onTestStart method " + getTestMethodName(iTestResult) + " start");
+		logger.info("I am in onTestStart method " + getTestMethodName(iTestResult) + " start");
 		ExtentTestManager.startTest(iTestResult.getMethod().getMethodName(), "");
 	}
 
 	public void onTestSuccess(ITestResult iTestResult) {
-		System.out.println("I am in onTestSuccess method " + getTestMethodName(iTestResult) + " succeed");
-		// Extentreports log operation for passed tests.
-		ExtentTestManager.getTest().log(Status.PASS, "Test passed");
+		logger.info("I am in onTestSuccess method " + getTestMethodName(iTestResult) + " succeed");
+		ExtentTestManager.getTest().log(Status.PASS, "Test passed.");
 
 	}
 
 	public void onTestFailure(ITestResult iTestResult) {
-		System.out.println("I am in onTestFailure method " + getTestMethodName(iTestResult) + " failed");
+		logger.error("I am in onTestFailure method " + getTestMethodName(iTestResult) + " failed");
 		ExtentTestManager.getTest().log(Status.FAIL, "Test Step Failed is " + iTestResult.getThrowable());
 	}
 
 	public void onTestSkipped(ITestResult iTestResult) {
-		System.out.println("I am in onTestSkipped method " + getTestMethodName(iTestResult) + " skipped");
-		// Extentreports log operation for skipped tests.
-		ExtentTestManager.getTest().log(Status.SKIP, "Test Skipped");
+		logger.warn("I am in onTestSkipped method " + getTestMethodName(iTestResult) + " skipped");
+		ExtentTestManager.getTest().log(Status.SKIP, "Test Skipped.");
 	}
 
 	public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
-		System.out.println("Test failed but it is in defined success ratio " + getTestMethodName(iTestResult));
+		logger.warn("Test failed but it is in defined success ratio " + getTestMethodName(iTestResult));
 	}
 
 }
