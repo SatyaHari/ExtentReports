f96d9e753a9b6166e2dfe16dd56ca6e7b1d415cc
Dipan Garg <dipangarg92@gmail.com>
2019-01-17 12:45:27 +0530

    klov reporter functionality added

--- src/main/java/org/cts/hybrid/extentreports/ExtentConfiguration.java
+++ src/main/java/org/cts/hybrid/extentreports/ExtentConfiguration.java
@@ -7,7 +7,7 @@ import java.util.logging.Logger;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+import com.aventstack.extentreports.reporter.ExtentKlovReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtentConfiguration {
@@ -27,7 +27,7 @@ public class ExtentConfiguration {
 	public static ExtentReports getInstance() {
 		if (extent == null) {
 			createReportsFolder();
-			createInstance();
+			attachReporters();
 		}
 		return extent;
 	}
@@ -39,17 +39,29 @@ public class ExtentConfiguration {
 		}
 	}
 
-	private static ExtentReports createInstance() {
+	private static ExtentHtmlReporter initHtmlReporter() {
 		ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(EXTENT_REPORTS_PATH);
-		htmlReporter.config().setChartVisibilityOnOpen(true);
-		htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
 		htmlReporter.config().setTheme(Theme.STANDARD);
 		htmlReporter.config().setDocumentTitle(REPORT_NAME);
 		htmlReporter.config().setEncoding("utf-8");
 		htmlReporter.config().setReportName("Execution-Status");
-		htmlReporter.setAppendExisting(true);
+		// htmlReporter.setAppendExisting(true);
+		return htmlReporter;
+	}
+
+	private static ExtentKlovReporter initKlovReporter() {
+		ExtentKlovReporter klovReporter = new ExtentKlovReporter("OneFramework", "BNYM");
+		klovReporter.initMongoDbConnection("localhost", 27017);
+		klovReporter.setReportName("Extent-klov-report");
+		klovReporter.initKlovServerConnection("http://localhost");
+		return klovReporter;
+	}
+
+	private static ExtentReports attachReporters() {
 		extent = new ExtentReports();
-		extent.attachReporter(htmlReporter);
+		extent.attachReporter(initKlovReporter());
+		extent.attachReporter(initHtmlReporter());
 		return extent;
 	}
+
 }
