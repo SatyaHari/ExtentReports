c33762d2757a297391c2b08f339b8b8194bd2276
491073 <491073@LT096911.cts.com>
2019-01-26 17:25:17 +0530

    bug fixes

--- src/main/java/org/cts/oneframework/extentreports/ExtentTestManager.java
+++ src/main/java/org/cts/oneframework/extentreports/ExtentTestManager.java
@@ -1,8 +1,5 @@
 package org.cts.oneframework.extentreports;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 
@@ -11,29 +8,21 @@ public class ExtentTestManager {
 	private ExtentTestManager() {
 	}
 
-	private static int counter = 0;
-	private static Map<Integer, ExtentTest> extentTestMap = new HashMap<>();
 	private static ExtentReports extent;
+	private static ThreadLocal<ExtentTest> extentTest = new ThreadLocal<>();
 
 	public static synchronized ExtentTest getTest() {
-		return extentTestMap.get(getCurrentThread());
+		return extentTest.get();
 	}
 
 	public static synchronized void endTest() {
-		if (!extentTestMap.isEmpty())
-			extent.removeTest(extentTestMap.get(getCurrentThread()));
+		if (extentTest.get() != null)
+			extent.removeTest(extentTest.get());
 	}
 
 	public static synchronized ExtentTest startTest(String testName, final String desc) {
 		extent = ExtentConfiguration.getInstance();
-		counter++;
-		ExtentTest test = extent.createTest(testName + "_" + counter, desc);
-		extentTestMap.put(getCurrentThread(), test);
-		return test;
+		extentTest.set(extent.createTest(testName + Thread.currentThread().getId(), desc));
+		return extentTest.get();
 	}
-
-	private static synchronized int getCurrentThread() {
-		return (int) (Thread.currentThread().getId());
-	}
-
 }
